422b4faa4e72b2e25c75896008b3a0341bc92808
danielmekuriaw <daniel.mekuriaw16@gmail.com>
2021-07-22 23:26:21 -0500

    Looping code-along DONE

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+
+function writeCards(array, text){
+    let result = []
+    for(let index = 0; index < array.length; index++){
+        result.push(`Thank you, ${array[index]}, for the wonderful ${text} gift!`)
+    }
+
+    return result
+}
+
+function countDown(number){
+    while(number >= 0){
+        console.log(number)
+        number = number - 1
+    }
+}
